13b1e2e3a8ab23e8f7a36c63e9302717b82ae14a
Oskar Niburski <oskarniburski@gmail.com>
2013-10-29 15:01:19 -0400

    Weather Scraper works, now just css

--- datavisual.rb
+++ datavisual.rb
@@ -10,8 +10,23 @@ File.open('weather.txt', 'w') do |f|
 	puts weather.text
 end
 =end
-page=Nokogiri::HTML(open("http://www.timeanddate.com/weather/canada/hamilton"))
-#puts page
-currentweather=page.css("div div table.tbbox tr.d1 table.rpad tr")[1].css("td")[1]
-puts currentweather.text
-puts page.css("div div table.tbbox tr.d0 table tbody tr.c0")[1].text
+File.open('weather.html', 'w') do |f|
+	f.write("<!DOCTYPE html>\n<head>\n<title> Weather Deconstructed </title>\n<link rel= \"stylesheet\" href=\"stylesheetweather.css\">\n</head>\n<body>\n<header>\n<h1> Weather for Hamilton, Ontario </h1>\n<hr>\n</header>\n<div>\n")
+	page=Nokogiri::HTML(open("http://www.timeanddate.com/weather/canada/hamilton"))
+	#puts page
+	currentweather=page.css("div div table.tbbox tr.d1 table.rpad tr")[1].css("td")[1].text
+	time=page.css("div div table.tbbox tr.d0 thead tr.c1")[1].text
+	futureweather=page.css("div div table.tbbox tr.d0 table tbody tr.c0")[1].text
+	f.write("<p> Today's temperature: "+currentweather[0]+" Celsuis"+"</p>\n")
+	time=time.scan(/[A-Z][a-z]+/)
+
+	futureweather=futureweather.scan(/\d+/)
+	combinedtimeandweather=time.zip(futureweather)
+
+	combinedtimeandweather.each_with_index do |i|
+		f.write("<p>"+i[0]+" temperature: "+i[1]+" Celsius</p>\n")
+	end
+	f.write("</div>\n<footer>\n<hr>\n<p> Contact me if you are interested in weather </p>\n<p> 905-575-5111 </p>\n<p> skype: oskarniburski </p>\n<a href=\"mailto:oskarniburski@gmail.com\">oskarniburski@gmail.com</a>\n</footer>\n</body>\n")
+end
+
+
