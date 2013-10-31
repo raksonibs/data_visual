2b05cb0e7bc9ad07b259d698113a8d5245a2edb3
Oskar Niburski <oskarniburski@gmail.com>
2013-10-31 10:55:13 -0400

    Graph formed, formating made

--- datavisual.rb
+++ datavisual.rb
@@ -14,9 +14,10 @@ end
 
 #create css file also to get based on current description, cloudy, sunny, rainy, a new image.
 #want to make the css background image change depending on the temperature range
+#change to farenheit? Fun facts about hamilton
 File.open('weather.html', 'w') do |f|
 
-	f.write("<!DOCTYPE html>\n<html>\n<head>\n<title> Weather Deconstructed </title>\n<link rel= \"stylesheet\" href=\"stylesheetweather1.css\">\n</head>\n<body>\n<script src=\"javascriptweather.js\"></script>\n<header>\n<h1> Weather for Hamilton, Ontario </h1>\n<hr>\n</header>\n<div>\n")
+	f.write("<!DOCTYPE html>\n<html>\n<head>\n<title> Weather Deconstructed </title>\n<link rel= \"stylesheet\" href=\"stylesheetweather1.css\">\n</head>\n<body>\n<script src=\"javascriptweather.js\"></script>\n<header>\n<h1> Weather for Hamilton, Ontario </h1>\n</header>\n<div>\n<hr>\n")
 	page=Nokogiri::HTML(open("http://www.timeanddate.com/weather/canada/hamilton"))
 	#puts page
 
@@ -77,7 +78,7 @@ File.open('weather.html', 'w') do |f|
 		f.write("<p>"+i[0]+" temperature: "+i[1]+" Celsius</p>\n")
 	end
 =end
-	f.write("<img class=\"chart\"src=#{chart}>\n</div>\n<footer>\n<hr>\n<p> Contact me if you are interested in weather </p>\n<p> 905-575-5111 </p>\n<p> skype: oskarniburski </p>\n<a href=\"mailto:oskarniburski@gmail.com\">oskarniburski@gmail.com</a>\n</footer>\n</body>\n</html>")
+	f.write("\n</div>\n<div class= \"graph\">\n<img class=\"chart\"src=#{chart}>\n</div>\n<footer>\n<hr>\n<p> Contact me if you are interested in weather </p>\n<p> 905-575-5111 </p>\n<p> skype: oskarniburski </p>\n<a href=\"mailto:oskarniburski@gmail.com\">oskarniburski@gmail.com</a>\n</footer>\n</body>\n</html>")
 	File.open("stylesheetweather1.css", "w") do |c|
 
 		pic="clouds.jpg" if currentweatherdesc.match(/cloudy/i)
@@ -85,8 +86,9 @@ File.open('weather.html', 'w') do |f|
 		pic="rain.jpg" if currentweatherdesc.match(/rain/i) || currentweatherdesc.match(/shower/i)
 		pic="sunny.jpg" if currentweatherdesc.match(/sun/i) && !currentweatherdesc.match(/cloud/i)
 		pic="fog.jpg" if currentweatherdesc.match(/fog/i)
+		pic="sprinkles.jpg" if currentweatherdesc.match(/sprinkles/i)
 
-		c.write("html {\n background: url(#{pic}) no-repeat center center fixed;\n-webkit-background-size: cover;\n-moz-background-size: cover;\n-o-background-size: cover;\nbackground-size: cover;\n}\nimg#chart {\nmargin:auto;\n}\n* {\ncolor: #660099;\nfont-size: 25px;\n} \nh2,\nem.head {\n font-size:46px;\nline-height:10%;\n}\nfooter p,\nfooter a,\na{\ncolor: red;\nline-height:10%;\n}\ndiv p {\n margin: auto;\ntext-align:center;\nopacity:0.4;\nfilter:alpha(opacity=40);\n}\ndiv p:hover {\nopacity:1.0;\nfilter:alpha(opacity=100);\n}\ndiv p:nth-child(even) {\ncolor:white;\nbackground-color:black;\n}\ndiv p:nth-child(odd) {\ncolor:black;\nbackground-color:white;\n}")
+		c.write("html {\n background: url(#{pic}) no-repeat center center fixed;\n-webkit-background-size: cover;\n-moz-background-size: cover;\n-o-background-size: cover;\nbackground-size: cover;\n}.graph {\nbackground-color: rgba(120,32,32,0.5);\nborder: 1px solid black;\n}\nimg.chart {\nmargin:auto;\n}\n* {\ncolor: #660099;\nfont-size: 25px;\n} \nh2,\nem.head {\n font-size:46px;\nline-height:10%;\n}\nfooter p,\nfooter a,\na{\ncolor: red;\nline-height:10%;\n}\ndiv p {\n margin: auto;\ntext-align:center;\nopacity:0.4;\nfilter:alpha(opacity=40);\nborder-radius:4%;\nwidth: 60vw;\n}\ndiv p:hover {\nopacity:1.0;\nfilter:alpha(opacity=100);\n}\ndiv p:nth-child(even) {\ncolor:white;\nbackground-color:black;\n}\ndiv p:nth-child(odd) {\ncolor:black;\nbackground-color:white;\n}\ndiv:not(.graph) {\nbackground-color: rgba(276,34,54,0.5);\nborder-radius: 5%;\nwidth:80vw;\n}")
 	end
 end
 
